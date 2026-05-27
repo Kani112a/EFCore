0e450f1bd73fd87f1304636ca86d5e9bb813f0aa
kanishka-d <kanishkadurai03@gmail.com>
2026-05-27 15:12:26 +0530

    Initial Commit

---
+++ EmployeeManagementEFCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeManagementEFCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementEFCore.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
